aa4a8264241d76e384f6c2601eb6e4ee9eda39a0
Megane173 <carlosdavidcotedominguez@gmail.com>
2025-10-23 21:48:17 -0500

    Version 2
    Ahora tiene menu para 2 opciones, saber si un numero es par o impar; y saber si es positivo o negativo o neutro

--- MVC/controlador.py
+++ MVC/controlador.py
@@ -1,5 +1,6 @@
 
 
+
 class Controlador:
 
     def __init__(self, modelo="", vista=""):
@@ -15,13 +16,35 @@ class Controlador:
         return self.vista
     def set_vista(self, vista):
         self.vista=vista
+        
+    def mostrar_menu(self):
+        
+        eleccion=self.vista.mostrar_menu()
+        if(eleccion=="-1"):
+            self.vista.imprimir("Saliendo...")
+        elif(eleccion!=1 or eleccion!=2):
+            self.vista.imprimir("\nDigito un caracter que no esta asociado a ninguna opcion.")
+        else:
+            self.completar_accion(eleccion)
+            
+    def pedir_numero(self):
+        numero=self.vista.hacer_campo()
+        self.modelo.set_numero(numero)
+        
+    def completar_accion(self, eleccion):
+        
+        self.pedir_numero()
+        
+        msg="abd"
+        
+        if(eleccion=="1"):
+            msg=self.modelo.es_par()
+        elif(eleccion=="2"):
+            msg=self.modelo.es_positivo()  
+        msg+="\n"
+        
+        self.mostrar_resultado(msg)
+        
 
-    def construir_pregunta(self):
-
-        dato_numero=self.vista.hacer_campo()
-        self.modelo.set_numero(dato_numero)
-
-    def validar_numero(self):
-
-        msg=self.modelo.validar_numero()
+    def mostrar_resultado(self, msg):
         self.vista.imprimir(msg)
